4e00590f7adbe066e50d1dd4f66b2f04cc80045c
박용찬 <parkyongchan@naver.com>
2026-06-06 18:00:07 +0900

    fix(large-msg): inboxFastSync 제거 - BROAD 폴링이 ~R: ACK 송신과 BLE 경쟁해 에코 타임아웃 유발(MT 수신 방해). doneFix5/txGapFix2 유지

--- MessageBox/app/src/main/java/com/ah/acr/messagebox/MainActivity.java
+++ MessageBox/app/src/main/java/com/ah/acr/messagebox/MainActivity.java
@@ -127,7 +127,6 @@ public class MainActivity extends AppCompatActivity {
     private static final long BROAD_TIMEOUT_MS = 15000;
     private static final long INFO_TIMEOUT_MS = 8000;
     private static final long PERIODIC_SYNC_MS = 30000;
-    private static final long INBOX_FAST_SYNC_MS = 3000;   // [inboxFastSync] inbox>0이면 빠른 BROAD 폴링 (BLE 로컬, 위성비용 0)
 
     private boolean mIsAutoReceiving = false;
     private int mLastInboxCount = 0;
@@ -820,8 +819,7 @@ public class MainActivity extends AppCompatActivity {
                 BLE.INSTANCE.getWriteQueue().offer("BROAD=5");
             }
 
-            // [inboxFastSync] inbox 남았으면 3초, 비면 30초
-            mSyncHandler.postDelayed(this, mLastInboxCount > 0 ? INBOX_FAST_SYNC_MS : PERIODIC_SYNC_MS);
+            mSyncHandler.postDelayed(this, PERIODIC_SYNC_MS);
         }
     };
 
